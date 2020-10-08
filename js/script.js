e5391bf434f05932667444081704462bf805f860
Vladyslav Pryhara <vladikpr94@hotmail.com>
2020-10-08 14:35:34 +0200

    first lesson: Variables declaration

---
+++ js/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+//Variables declaration
+let money,
+    income,
+    addExpenses,
+    deposit,
+    mission,
+    period;
+
+
+alert('Hello World!');
+console.log("JS is working");
