04546724a508e4b7e6f5dfc81a69d1f7de935243
Vladyslav Pryhara <vladikpr94@hotmail.com>
2020-10-09 21:34:02 +0200

    Second lesson

--- js/script.js
+++ js/script.js
@@ -2,13 +2,28 @@
 
 
 //Variables declaration
-let money,
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
+let money = 1200,
+    income = 'Freelance',
+    addExpenses = 'Social Insurance, Internet, Foodstuff',
+    deposit = true,
+    mission = 10000,
+    period = 8,
+    budgetDay = money/30;
 
 
-alert('Hello World!');
-console.log("JS is working");
+//alert('Hello World!');
+//console.log("JS is working");
+
+// Second task
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+
+console.log(addExpenses.length);
+
+console.log(`Период равен ${period}`);
+console.log("Цель заработать " + mission +" евро");
+
+console.log(addExpenses.toLowerCase().split(", "));
+
+console.log(budgetDay.toFixed());
